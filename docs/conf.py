faf970916aca34eb22f1b330e454efba774d9371
Jorge Velazquez-Castro <jorge.velazquezcastro@correo.buap.mx>
2017-02-13 21:26:31 -0600

    docs build commited in order to test githubpages

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax']
+#    'sphinx.ext.viewcode']
+
     'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
